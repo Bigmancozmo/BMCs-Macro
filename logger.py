090963e2f6c2ea2e0d63f161c17ba62a4cc65c5b
Bigmancozmo <bigmancozmo@proton.me>
2025-07-27 20:13:40 -0400

    idk

--- logger.py
+++ logger.py
@@ -3,9 +3,16 @@ import sys
 
 class Logger:
     def __init__(self, filename):
-        self.terminal = sys.stdout
         self.log = open(filename, "w")
+        self.terminal = sys.__stdout__ if sys.__stdout__ is not None else sys.stdout
+        if self.terminal is None:
+            # no console, fallback to something dumb that just swallows writes
+            class Dummy:
+                def write(self, msg): pass
+                def flush(self): pass
+            self.terminal = Dummy()
         print("[Logger]: Created logger")
+
     def write(self, message):
         self.terminal.write(message)
         self.log.write(message)
